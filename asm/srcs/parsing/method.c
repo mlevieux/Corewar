28104d08a56ef9764165874ea65c917b976898fd
jb255 <jaustry@student.42.fr>
2016-11-15 18:16:47 +0100

    comptage position ok

--- asm/srcs/parsing/method.c
+++ asm/srcs/parsing/method.c
@@ -19,7 +19,8 @@ int		coerence_str_optab(char *str, t_env *e) // verifie si le type
 		e->method_total += 1;
 		return (1);
 	}
-	else if ((str[0]) == '%' && ((str[1] == ':') || (ft_strlen(str) < 5)))
+	else if ((str[0]) == '%' && ((str[1] == ':') ||
+		((ft_strlen(str) < 5) && str[1] != '0')))
 	{
 		e->method_total += 2;
 		return (10);
